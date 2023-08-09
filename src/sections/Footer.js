f6ec75a8621817eee1ee257c98b9de0c4acaff46
Oluwatobi metibemu <metibemuoluwatobi2@gmail.com>
2023-08-09 16:27:24 +0100

    I added the footer

---
+++ src/sections/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div class="foot">
+
+<div class="section">
+    <p>Connect</p>
+    <p class="paragraphs">
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollitAmet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit
+    </p>
+</div>
+
+<div class="section">
+    <p>Products</p>
+    <div class="paragraphs">
+        <p>Overview</p>
+        <p>Feature</p>
+        <p>Solutions</p>
+        <p>Tutorials</p>
+        <p>Pricing</p>
+    </div>
+        
+</div>
+
+
+<div class="section">
+    <p>Company</p>
+    <div class="paragraphs">
+        <p>About us</p>
+        <p>Contacts</p>
+        <p>Press</p>
+        <p>News</p>
+        <p>Careers</p>
+    </div>
+</div>
+
+<div class="section">
+    <p>Legal</p>
+    <div class="paragraphs">
+        <p>Terms and Services</p>
+        <p>Privacy</p>
+        <p>Support</p>
+        <p>License</p>
+    </div>
+</div>
+</div>
+    </div>
+  )
+}
+
+export default Footer
