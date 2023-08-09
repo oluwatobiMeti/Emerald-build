ba418e7b9c439837d53878f2bf1cd7824e053a4e
davidmicah123 <micahdanielsd@gmail.com>
2023-08-09 16:39:00 +0100

    Added about component

---
+++ src/Pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../component/Nav";
+import Leagues from "../sections/Leagues";
+
+function About() {
+  return (
+    <div>
+      <Nav />
+      <Leagues />
+    </div>
+  );
+}
+
+export default About;
