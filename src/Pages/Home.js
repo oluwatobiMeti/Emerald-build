263ed05e78c7b8e1e9261a0da017cddec1e90d4c
Oluwatobi metibemu <metibemuoluwatobi2@gmail.com>
2023-08-09 16:47:41 +0100

    resolved a conflict

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,26 +1,17 @@
-<<<<<<< HEAD
 import React from 'react'
 import Nav from '../component/Nav'
 import Leagues from '../sections/Leagues';
 import Hero from '../component/Hero';
 import Footer from '../sections/Footer';
 
-=======
-import React from 'react';
-import Nav from '../component/Nav';
-import Leagues from '../sections/Leagues';
-import Hero from '../component/Hero';
->>>>>>> aaa7d8b2d22eb651d9b12b2328c8c281624ad597
+
 function Home() {
   return (
     <div>
       <Nav/>
       <Hero/>
       <Leagues />
-<<<<<<< HEAD
       <Footer/>
-=======
->>>>>>> aaa7d8b2d22eb651d9b12b2328c8c281624ad597
     </div>
   )
 }
