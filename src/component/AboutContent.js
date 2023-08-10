b806d212dee9af43db9beb19c956714d20492eff
Oluwatobi metibemu <metibemuoluwatobi2@gmail.com>
2023-08-10 13:41:54 +0100

    modified the style

--- src/component/AboutContent.js
+++ src/component/AboutContent.js
@@ -5,7 +5,7 @@ export default function AboutContent() {
     <div>
         <div class="imageHolder">
             <div class="TextHolder">
-            <h3>Get in touch</h3>
+            <h3 className='textHeolder__header'>Get in touch</h3>
             <p class="text">To Get Informations About Football</p>
             </div>
         </div>
