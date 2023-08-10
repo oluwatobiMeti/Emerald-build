988781fc73761f986bd77cc432424427f6622bc0
davidmicah123 <micahdanielsd@gmail.com>
2023-08-10 17:29:40 +0100

    Update the Home page

---
+++ src/Data/League1Teams.js
@@ -0,0 +1,104 @@
+export const Ligue1Teams = [
+  {
+    teamName: "Paris Saint-Germain",
+    teamFlag: "https://example.com/psg_flag.png",
+    overview:
+      "Paris Saint-Germain, commonly known as PSG, is one of the most successful and high-profile football clubs in France. They have won multiple Ligue 1 titles and compete at the highest level of European competitions.",
+  },
+  {
+    teamName: "Olympique de Marseille",
+    teamFlag: "https://example.com/marseille_flag.png",
+    overview:
+      "Olympique de Marseille is a storied French club with a passionate fan base. They have a rich history in domestic and European competitions, and their matches are known for their intense atmosphere.",
+  },
+  {
+    teamName: "Olympique Lyonnais",
+    teamFlag: "https://example.com/lyon_flag.png",
+    overview:
+      "Olympique Lyonnais, often referred to as Lyon, is recognized for its strong academy and attractive style of play. They have won numerous Ligue 1 titles and have a successful track record in Europe.",
+  },
+  {
+    teamName: "AS Monaco",
+    teamFlag: "https://example.com/monaco_flag.png",
+    overview:
+      "AS Monaco is known for its emphasis on developing young talents and playing attacking football. They have a history of challenging for the Ligue 1 title and have also reached the UEFA Champions League final.",
+  },
+  {
+    teamName: "Lille OSC",
+    teamFlag: "https://example.com/lille_flag.png",
+    overview:
+      "Lille OSC, based in Lille, has a reputation for nurturing talents and competing effectively in Ligue 1. They secured a surprising Ligue 1 title victory in recent years.",
+  },
+  {
+    teamName: "Stade Rennais FC",
+    teamFlag: "https://example.com/rennes_flag.png",
+    overview:
+      "Stade Rennais FC, commonly known as Rennes, is a competitive club with a focus on developing young players. They aim to challenge the top teams in Ligue 1.",
+  },
+  {
+    teamName: "OGC Nice",
+    teamFlag: "https://example.com/nice_flag.png",
+    overview:
+      "OGC Nice, based in Nice, aims to play attractive football and challenge for European qualification spots. They have a loyal fan base and a solid presence in Ligue 1.",
+  },
+  {
+    teamName: "Montpellier HSC",
+    teamFlag: "https://example.com/montpellier_flag.png",
+    overview:
+      "Montpellier HSC, known for their unity and teamwork, achieved a surprise Ligue 1 title victory in the past. They continue to be a competitive force in the league.",
+  },
+  {
+    teamName: "Stade de Reims",
+    teamFlag: "https://example.com/reims_flag.png",
+    overview:
+      "Stade de Reims is a traditional French club with a history of success. They have achieved domestic and European milestones and look to build on their legacy.",
+  },
+  {
+    teamName: "RC Lens",
+    teamFlag: "https://example.com/lens_flag.png",
+    overview:
+      "RC Lens, based in Lens, has returned to Ligue 1 after periods in lower divisions. They aim to establish themselves and compete at a high level.",
+  },
+  {
+    teamName: "FC Nantes",
+    teamFlag: "https://example.com/nantes_flag.png",
+    overview:
+      "FC Nantes is a historic club with a rich heritage in French football. They have experienced periods of success and continue to be a respected team.",
+  },
+  {
+    teamName: "Angers SCO",
+    teamFlag: "https://example.com/angers_flag.png",
+    overview:
+      "Angers SCO, based in Angers, competes in Ligue 1 and focuses on solid team play. They aim to achieve consistent performances and growth.",
+  },
+  {
+    teamName: "Strasbourg",
+    teamFlag: "https://example.com/strasbourg_flag.png",
+    overview:
+      "Strasbourg, also known as Racing Club de Strasbourg Alsace, has a rich history in French football. They strive to compete at a high level in Ligue 1.",
+  },
+  {
+    teamName: "FC Metz",
+    teamFlag: "https://example.com/metz_flag.png",
+    overview:
+      "FC Metz, based in Metz, is a competitive team in Ligue 1. They aim to establish a strong presence and challenge top opponents.",
+  },
+  {
+    teamName: "AS Saint-Étienne",
+    teamFlag: "https://example.com/saint_etienne_flag.png",
+    overview:
+      "AS Saint-Étienne, often referred to as ASSE, is a historically significant club in French football. They have a dedicated fan base and a rich legacy.",
+  },
+  {
+    teamName: "Brest",
+    teamFlag: "https://example.com/brest_flag.png",
+    overview:
+      "Brest, based in Brest, competes in Ligue 1 and focuses on consistent performances. They aim to make their mark in French football.",
+  },
+  {
+    teamName: "FC Lorient",
+    teamFlag: "https://example.com/lorient_flag.png",
+    overview:
+      "FC Lorient, based in Lorient, is known for its commitment to youth development. They strive to compete strongly in Ligue 1.",
+  },
+];
---
+++ src/Pages/Gallery.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Gallery = () => {
+    return(
+        <div></div>
+    );
+};
+
+export default Gallery;
