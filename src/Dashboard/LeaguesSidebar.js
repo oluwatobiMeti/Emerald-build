fdf50fe77848d72520e2adc185dea25c9ad14bef
davidmicah123 <micahdanielsd@gmail.com>
2023-08-10 10:45:47 +0100

    Started developing the dashboars

---
+++ src/Dashboard/LeaguesSidebar.js
@@ -0,0 +1,12 @@
+import react from "react";
+import classes from "LeagueSidebar.module.css";
+
+const LeagueSidebar = () => {
+    return(
+        <div className={classes.league_sidebar}>
+            
+        </div>
+    );
+}
+
+export default LeagueSidebar;
