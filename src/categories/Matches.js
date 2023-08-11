cb5de72d977b2fbd85e694683fbe9698c3125eab
Oluwatobi metibemu <metibemuoluwatobi2@gmail.com>
2023-08-11 10:42:29 +0100

    I added the categories

--- src/categories/Matches.js
+++ src/categories/Matches.js
@@ -5,7 +5,7 @@ function Matches({League, date}) {
   return (
     <div>
       <h1>{date}</h1>
-      <Match club1 = {League.matches.team1} club1Icon = {} club2Icon = {} club2 = {League.matches.team2} goal1 = {League.matches.score.ft[0]} goal2 = {League.matches.score.ft[0]}/>
+      <Match club1 = {League.matches.team1} club1Icon = {""} club2Icon = {""} club2 = {League.matches.team2} goal1 = {League.matches.score.ft[0]} goal2 = {League.matches.score.ft[0]}/>
     </div>
 
   )
